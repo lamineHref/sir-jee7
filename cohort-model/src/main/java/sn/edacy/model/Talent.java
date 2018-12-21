602c607d091f23d76439dc15a43d69548a6606a5
lamineHref <44919479+lamineHref@users.noreply.github.com>
2018-12-21 14:25:34 +0000

    Update Talent.java

--- cohort-model/src/main/java/sn/edacy/model/Talent.java
+++ cohort-model/src/main/java/sn/edacy/model/Talent.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-/**test***/
+
 @Entity
 @Table(name = "talents")
 public class Talent {
